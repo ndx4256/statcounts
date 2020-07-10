d48948320be5a1bed949d855292d25cfcac3d5ee
ndx4256 <68079850+ndx4256@users.noreply.github.com>
2020-07-10 11:34:50 -0600

    cors

---
+++ cors.js
@@ -0,0 +1,9 @@
+	var cors = [
+	"https://api.allorigins.win/raw?url=",
+	"https://cors-anywhere.herokuapp.com/",
+	"https://non-cors.herokuapp.com/",
+	"https://unique-shoe.glitch.me/",
+  "https://api.allorigins.win/raw?url=",
+	"https://api.allorigins.win/raw?url=",
+  "https://api.allorigins.win/raw?url="
+]
