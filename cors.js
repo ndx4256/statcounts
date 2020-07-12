28368ae4e70b68c55e9eec06dcfbf5974982db0e
ndx <68079850+ndx4256@users.noreply.github.com>
2020-07-11 18:48:05 -0600

    Update cors.js

--- cors.js
+++ cors.js
@@ -1,7 +1,7 @@
 	var cors = [
 	"https://api.allorigins.win/raw?url=",
 	"https://cors-anywhere.herokuapp.com/",
-	"https://non-cors.herokuapp.com/",
+	
 	"https://unique-shoe.glitch.me/",
   "https://api.allorigins.win/raw?url=",
 	"https://api.allorigins.win/raw?url=",
