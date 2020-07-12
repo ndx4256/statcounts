432160978f8abbffff8c6f754a21cdf52a252863
ndx <68079850+ndx4256@users.noreply.github.com>
2020-07-12 09:31:34 -0600

    Delete cors.js

--- cors.js
+++
@@ -1,9 +0,0 @@
-	var cors = [
-	"https://api.allorigins.win/raw?url=",
-	"https://cors-anywhere.herokuapp.com/",
-	
-	"https://unique-shoe.glitch.me/",
-  "https://api.allorigins.win/raw?url=",
-	"https://api.allorigins.win/raw?url=",
-  "https://api.allorigins.win/raw?url="
-]
